4c5ff272b25353cf2bb45af0dac65bf78dab15b2
Manal <manalhdkz@gmail.com>
2021-04-04 22:41:05 +0200

    update charts url to https

--- week4/hackyourweather/src/App.js
+++ week4/hackyourweather/src/App.js
@@ -1,22 +1,12 @@
 import './App.css';
 import React from 'react';
 import Main from './component/Main'
-import City from './component/City'
-import Search from './component/Search';
-import {TiWeatherPartlySunny} from 'react-icons/ti'
 import Charts from './component/Charts'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link,
-  Redirect,
-  useHistory,
-  useParams,
-  useLocation
 } from "react-router-dom";
-import { FaRegCaretSquareDown } from 'react-icons/fa';
-
 
 const PageNotFound = () =>{
   return <h2>This page is not found</h2>;
@@ -31,7 +21,6 @@ function App() {
             <Main />
         </Route>
          <Route path="/:cityId" >
-            
             <Charts/>
          </Route>
         <Route path="*">
--- week4/hackyourweather/src/component/Charts.js
+++ week4/hackyourweather/src/component/Charts.js
@@ -25,7 +25,7 @@ const Charts = () => {
      
       const fetchChart = async() =>{
           try{
-            const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?id=${cityId}&appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}`)
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?id=${cityId}&appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}`)
             const data = await response.json()
             setCityName(data.city)
             setupChart({...data})
